13e6209f59072adc266daa0e3a1c57ee23c9b5a0
Rafael Vieira Suarez <rafael.suarez@compasso.com.br>
2023-04-19 00:11:29 -0300

    primeiro commit

---
+++ MauiBuberBrakefastNiceLayout/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiBuberBrakefastNiceLayout;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
