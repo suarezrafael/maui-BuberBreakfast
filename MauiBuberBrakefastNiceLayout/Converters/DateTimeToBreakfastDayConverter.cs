e2946fa19237db20e150fec849484344e59cb4b7
Rafael Vieira Suarez <rafael.suarez@compasso.com.br>
2023-04-19 23:42:38 -0300

    adicionado converter de data e hora.

---
+++ MauiBuberBrakefastNiceLayout/Converters/DateTimeToBreakfastDayConverter.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Maui.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBuberBrakefastNiceLayout.Converters
+{
+    public class DateTimeToBreakfastDayConverter : BaseConverter<object, string>
+    {
+        public override string DefaultConvertReturnValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override object DefaultConvertBackReturnValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override object ConvertBackTo(string value, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ConvertFrom(object value, CultureInfo culture)
+        {
+            DateTime dateTime = (DateTime)value;
+            return (dateTime.Date - DateTime.Today).TotalDays switch
+            {
+                0 => "Today",
+                1 => "Tomorrow",
+                _ => $"{dateTime.DayOfWeek},{dateTime.Day}.{dateTime.Month}",
+            };
+
+        }
+    }
+}
--- MauiBuberBrakefastNiceLayout/MauiProgram.cs
+++ MauiBuberBrakefastNiceLayout/MauiProgram.cs
@@ -1,19 +1,17 @@
-﻿namespace MauiBuberBrakefastNiceLayout;
+﻿using CommunityToolkit.Maui;
 
+namespace MauiBuberBrakefastNiceLayout;
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
-            });
-
-		return builder.Build();
-	}
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
+        }).UseMauiCommunityToolkit();
+        return builder.Build();
+    }
 }
