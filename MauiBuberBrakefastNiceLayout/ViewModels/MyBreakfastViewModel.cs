02d091d168c4a049eeff35e3caa583dc51ba1c96
Rafael Vieira Suarez <rafael.suarez@compasso.com.br>
2023-04-19 23:06:09 -0300

    RefreshView

--- MauiBuberBrakefastNiceLayout/ViewModels/MyBreakfastViewModel.cs
+++ MauiBuberBrakefastNiceLayout/ViewModels/MyBreakfastViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using MauiBuberBrakefastNiceLayout.Models;
 using System;
 using System.Collections.Generic;
@@ -13,21 +14,30 @@ public partial class MyBreakfastViewModel : ObservableObject
     [ObservableProperty]
     List<Breakfast> breakfasts;
 
+    [ObservableProperty]
+    bool isRefreshing;
+
     public MyBreakfastViewModel()
     {
-        LoadBreakfasts();
+        LoadBreakfastsAsync();
     }
-
-    private void LoadBreakfasts()
+    [RelayCommand]
+    public async Task LoadBreakfastsAsync()
     {
-        Breakfasts = new() 
-        { 
+        try
+        {
+            IsRefreshing = true;
+
+            await Task.Delay(2000);
+
+            Breakfasts = new()
+        {
             new Breakfast(
-                Name:"Vegan Sunshine", 
+                Name:"Vegan Sunshine",
                 Description:"Vegan everything!",
                 StartDateTime:DateTime.UtcNow.AddDays(1),
                 EndDateTime:DateTime.UtcNow.AddDays(1).AddHours(2),
-                ImageUrl: new Uri("https://images/unsplash.com/photo1633204339708-86"),
+                ImageUrl: new Uri("https://images.unsplash.com/photo1633204339708-86"),
                 Savory: new List<string>{ "Oatmeal","Avocado Toast","Omelet","Salac"},
                 Sweet: new List<string>{"Cookie"}),
             new Breakfast(
@@ -35,11 +45,16 @@ public partial class MyBreakfastViewModel : ObservableObject
                 Description:"Vegan everything!",
                 StartDateTime:DateTime.UtcNow.AddDays(1),
                 EndDateTime:DateTime.UtcNow.AddDays(1).AddHours(2),
-                ImageUrl: new Uri("https://images/unsplash.com/photo1633204339708-86"),
+                ImageUrl: new Uri("https://images.unsplash.com/photo1633204339708-86"),
                 Savory: new List<string>{ "Oatmeal","Avocado Toast","Omelet","Salac"},
                 Sweet: new List<string>{"Cookie"})
 
         };
+        }
+        finally
+        {
+            IsRefreshing = false;
+        }
     }
 }
 
